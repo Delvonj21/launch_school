9926402e63dcb024a5f19257e1bc4da7f774f4a3
Delvon <delvonj21@gmail.com>
2024-09-12 08:22:07 +0200

    added files

---
+++ PY101-109/Easy2/retire.py
@@ -0,0 +1,6 @@
+# Build a program that displays when the user will retire and how many years she has to work till retirement.
+
+import datetime
+
+age = int(input("What is your age? "))
+retired_age = int(input("At what age would you like to retire? "))

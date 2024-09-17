c947c41cc75b1543b047e6902ee94c25d500d822
Delvon <delvonj21@gmail.com>
2024-09-17 08:43:52 +0200

    added files

--- PY101-109/Exercises/Easy3/daily_double.py
+++ PY101-109/Exercises/Easy3/daily_double.py
@@ -1,6 +1,19 @@
 # Write a function that takes a string argument and returns a new string that contains the value of the original string with all consecutive duplicate characters collapsed into a single character.
 
 
+def crunch(str):
+    if not str:
+        return ""
+
+    result = [str[0]]
+
+    for idx in range(1, len(str)):
+        if str[idx] != str[idx - 1]:
+            result.append(str[idx])
+
+    return "".join(result)
+
+
 # These examples should all print True
 print(crunch("ddaaiillyy ddoouubbllee") == "daily double")
 print(crunch("4444abcabccba") == "4abcabcba")

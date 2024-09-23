908b41466bf6c2efe7f29fb145b8d9734f513c7d
Delvon <delvonj21@gmail.com>
2024-09-23 13:48:48 +0200

    added files

--- PY101-109/Lesson_3/Hard/practice_problemsH1.py
+++ PY101-109/Lesson_3/Hard/practice_problemsH1.py
@@ -27,3 +27,62 @@ num_list.append(2)
 
 print(num_list)  # [1, 2]
 print(dictionary)  # {'first': [1,2]}
+
+# 3 Given the following similar sets of code, what will each code snippet print?
+
+# A
+
+
+def mess_with_vars(one, two, three):
+    one = two
+    two = three
+    three = one
+
+
+one = ["one"]
+two = ["two"]
+three = ["three"]
+
+mess_with_vars(one, two, three)
+
+print(f"one is: {one}")
+print(f"two is: {two}")
+print(f"three is: {three}")
+
+# B
+
+
+def mess_with_vars(one, two, three):
+    one = ["two"]
+    two = ["three"]
+    three = ["one"]
+
+
+one = ["one"]
+two = ["two"]
+three = ["three"]
+
+mess_with_vars(one, two, three)
+
+print(f"one is: {one}")
+print(f"two is: {two}")
+print(f"three is: {three}")
+
+# C
+
+
+def mess_with_vars(one, two, three):
+    one[0] = "two"
+    two[0] = "three"
+    three[0] = "one"
+
+
+one = ["one"]
+two = ["two"]
+three = ["three"]
+
+mess_with_vars(one, two, three)
+
+print(f"one is: {one}")
+print(f"two is: {two}")
+print(f"three is: {three}")

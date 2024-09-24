a4e3f8f43a1d4120df2db3830827102e11f62142
Delvon <delvonj21@gmail.com>
2024-09-24 13:17:35 +0200

    added files

--- PY101-109/Lesson_3/Hard/practice_problemsH1.py
+++ PY101-109/Lesson_3/Hard/practice_problemsH1.py
@@ -45,9 +45,9 @@ three = ["three"]
 
 mess_with_vars(one, two, three)
 
-print(f"one is: {one}")
-print(f"two is: {two}")
-print(f"three is: {three}")
+print(f"one is: {one}")  # "one is: ["one"]"
+print(f"two is: {two}")  # "two is: ["two"]"
+print(f"three is: {three}")  # "three is: ["three"]"
 
 # B
 
@@ -64,9 +64,9 @@ three = ["three"]
 
 mess_with_vars(one, two, three)
 
-print(f"one is: {one}")
-print(f"two is: {two}")
-print(f"three is: {three}")
+print(f"one is: {one}")  # "one is: ["one"]"
+print(f"two is: {two}")  # "two is: ["two"]"
+print(f"three is: {three}")  # "three is: ["three"]"
 
 # C
 
@@ -83,6 +83,26 @@ three = ["three"]
 
 mess_with_vars(one, two, three)
 
-print(f"one is: {one}")
-print(f"two is: {two}")
-print(f"three is: {three}")
+print(f"one is: {one}")  # "one is: ["two"]"
+print(f"two is: {two}")  # "two is: ["three"]"
+print(f"three is: {three}")  # "three is: ["one"]"
+
+
+# 4 Ben was tasked to write a simple Python function to determine whether an input string is an IP address using 4 dot-separated numbers, e.g., 10.4.5.11.
+
+# Alyssa supplied Ben with a function named is_an_ip_number. It determines whether a string is a numeric string between 0 and 255 as required for IP numbers and asked Ben to use it. Here's the code that Ben wrote:
+
+
+def is_dot_separated_ip_address(input_string):
+    dot_separated_words = input_string.split(".")
+    while len(dot_separated_words) > 0:
+        word = dot_separated_words.pop()
+        if not is_an_ip_number(word):
+            break
+
+    return True
+
+
+# Alyssa reviewed Ben's code and said, "It's a good start, but you missed a few things. You're not returning a false condition, and you're not handling the case when the input string has more or less than 4 components, e.g., 4.5.5 or 1.2.3.4.5: both those values should be invalid."
+
+# Help Ben fix his code.

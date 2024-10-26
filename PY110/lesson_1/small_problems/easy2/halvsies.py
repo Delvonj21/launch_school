adf5bbfd3570db09210e142719043b20343c6941
Delvon <delvonj21@gmail.com>
2024-10-26 14:44:24 +0200

    added files

---
+++ PY110/lesson_1/small_problems/easy2/halvsies.py
@@ -0,0 +1,1 @@
+# Write a function that takes a list as an argument and returns a list that contains two elements, both of which are lists. Put the first half of the original list elements in the first element of the return value and put the second half in the second element. If the original list contains an odd number of elements, place the middle element in the first half list.

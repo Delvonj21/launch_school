e2222ab72de2dab66db95792f1cc81662cc25bcf
Delvon <delvonj21@gmail.com>
2024-10-28 10:21:36 +0100

    added files

--- PY110/lesson_1/small_problems/easy2/halvsies.py
+++ PY110/lesson_1/small_problems/easy2/halvsies.py
@@ -1,1 +1,15 @@
 # Write a function that takes a list as an argument and returns a list that contains two elements, both of which are lists. Put the first half of the original list elements in the first element of the return value and put the second half in the second element. If the original list contains an odd number of elements, place the middle element in the first half list.
+
+
+def halvsies(lst):
+    middle_value = (len(lst) + 1) // 2
+    first_half = lst[:middle_value]
+    second_half = lst[middle_value:]
+    return [first_half, second_half]
+
+
+# All of these examples should print True
+print(halvsies([1, 2, 3, 4]) == [[1, 2], [3, 4]])
+print(halvsies([1, 5, 2, 4, 3]) == [[1, 5, 2], [4, 3]])
+print(halvsies([5]) == [[5], []])
+print(halvsies([]) == [[], []])

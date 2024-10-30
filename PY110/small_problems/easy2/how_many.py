bdf7a8da1a0d2f080df442acaf75d334611bebbd
Delvon <delvonj21@gmail.com>
2024-10-30 15:49:38 +0100

    added files

--- PY110/small_problems/easy2/how_many.py
+++ PY110/small_problems/easy2/how_many.py
@@ -1,24 +1,5 @@
 # Write a function that counts the number of occurrences of each element in a given list. Once counted, print each element alongside the number of occurrences. Consider the words case sensitive e.g. ("suv" != "SUV").
 
-# Input:
-# list
-# Output:
-# string
-# requirements:
-#  count the number of occurrences of each element in the list
-#  print each element alongside the number of occurrences
-# words are case sensitive
-
-
-# Data Structures: f-strings?
-
-# Algo
-# create a function with a list for an argument
-# create an empty dictionary
-# loop through the list and store words and their counts in the dictionary
-# make f strings from the dictionary data
-# return results
-
 
 def print_occurrences(word_counts):
     for key, value in word_counts.items():

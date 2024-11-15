04f26337227b504871584b0ccd411db0a190de36
Delvon <delvonj21@gmail.com>
2024-11-15 10:17:29 +0100

    added files / deleted files

---
+++ PY110/small_problems/easy1/convert_signed_number_to_string.py
@@ -0,0 +1,31 @@
+# In the previous exercise, you developed a function that converts non-negative numbers to strings. In this exercise, you're going to extend that function by adding the ability to represent negative numbers as well.
+
+# Write a function that takes an integer and converts it to a string representation.
+
+# You may not use any of the standard conversion functions available in Python, such as str. You may, however, use integer_to_string from the previous exercise.
+
+DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+
+def integer_to_string(number):
+    result = ""
+
+    while number > 0:
+        number, remainder = divmod(number, 10)
+        result = DIGITS[remainder] + result
+
+    return result or "0"
+
+
+def signed_integer_to_string(number):
+    if number < 0:
+        return f"-{integer_to_string(-number)}"
+    elif number > 0:
+        return f"+{integer_to_string(number)}"
+    else:
+        return "0"
+
+
+print(signed_integer_to_string(4321) == "+4321")  # True
+print(signed_integer_to_string(-123) == "-123")  # True
+print(signed_integer_to_string(0) == "0")  # True
--- PY110/small_problems/easy1/convert_string_number.py
+++ PY110/small_problems/easy1/convert_string_number.py
@@ -1,8 +1,6 @@
-# Write a function that takes a string of digits and returns the appropriate number as an integer. The string may have a leading + or - sign; if the first character is a +, your function should return a positive number; if it is a -, your function should return a negative number. If there is no sign, return a positive number.
+# Write a function that takes a string of digits and returns the appropriate number as an integer. You may not use any of the standard conversion functions available in Python, such as int. Your function should calculate the result by using the characters in the string.
 
-# You may assume the string will always contain a valid number.
-
-# You may not use any of the standard conversion functions available in Python, such as int. You may, however, use the string_to_integer function from the previous exercise.
+# For now, do not worry about leading + or - signs, nor should you worry about invalid characters; assume all characters are numeric.
 
 
 def string_to_integer(string):
@@ -26,16 +24,5 @@ def string_to_integer(string):
     return value
 
 
-def string_to_signed_integer(string):
-    match string[0]:
-        case "-":
-            return -string_to_integer(string[1:])
-        case "+":
-            return string_to_integer(string[1:])
-        case _:
-            return string_to_integer(string)
-
-
-print(string_to_signed_integer("4321") == 4321)  # True
-print(string_to_signed_integer("-570") == -570)  # True
-print(string_to_signed_integer("+100") == 100)  # True
+print(string_to_integer("4321") == 4321)  # True
+print(string_to_integer("570") == 570)  # True

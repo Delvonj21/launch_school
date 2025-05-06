9d396c3c5240688b9d24b4653ac87b657c0a40d6
Delvon <delvonj21@gmail.com>
2025-05-06 08:42:59 +0200

    added files

---
+++ PY120/small_problems/OO_basics3/mro2.py
@@ -0,0 +1,20 @@
+# Using the code below, determine the method resolution order (MRO) when accessing cat1.color. Only list the classes and mix-ins Python will check when searching for the color method.
+
+
+class Animal:
+    pass
+
+
+class Cat(Animal):
+    pass
+
+
+class Bird(Animal):
+    pass
+
+
+cat1 = Cat()
+cat1.color
+
+# Cat => Animal => object
+print(Cat.mro())

d2c76b77c4aa6f8b6922e636c2656121569365df
Delvon <delvonj21@gmail.com>
2025-05-12 08:49:12 +0200

    Added exercise files

--- PY120/small_problems/Easy/cats.py
+++ PY120/small_problems/Easy/cats.py
@@ -1,5 +1,6 @@
 # Update this code so you see the following output when you run the code:
 
+
 class Pet:
     def __init__(self, name, age):
         self._name = name
@@ -13,6 +14,7 @@ class Pet:
     def age(self):
         return self._age
 
+
 class Cat(Pet):
     def __init__(self, name, age, colors):
         super().__init__(name, age)
@@ -24,11 +26,13 @@ class Cat(Pet):
 
     @property
     def info(self):
-        return (f"My cat {self.name} is {self.age} "
-                f"years old and has {self.colors} fur.")
+        return (
+            f"My cat {self.name} is {self.age} " f"years old and has {self.colors} fur."
+        )
 
-cocoa = Cat('Cocoa', 3, 'black')
-cheddar = Cat('Cheddar', 4, 'yellow and white')
+
+cocoa = Cat("Cocoa", 3, "black")
+cheddar = Cat("Cheddar", 4, "yellow and white")
 
 print(cocoa.info)
 print(cheddar.info)

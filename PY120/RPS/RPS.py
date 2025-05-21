0400d772e00a68a38ecf998025137053b07d7ed9
Delvon <delvonj21@gmail.com>
2025-05-21 08:40:32 +0200

    worked on RPS game

--- PY120/RPS/RPS.py
+++ PY120/RPS/RPS.py
@@ -1,37 +1,95 @@
+import random
+
+
 class Player:
+    CHOICES = ("rock", "paper", "scissors")
+
     def __init__(self):
-        # maybe a "name"? what about a "move"?
-        pass
+        self.move = None
+
+
+class Computer:
+    def __init__(self):
+        self.move = None
 
     def choose(self):
-        pass
+        self.move = random.choice(Player.CHOICES)
 
 
-class Move:
+class Human:
     def __init__(self):
-        # This seems like we need something to keep track
-        # of the choice... a move object can be "paper", "rock" or "scissors"
-        pass
+        self.move = None
 
+    def choose(self):
+        prompt = "Please choose rock, paper, or scissors: "
 
-class Rule:
-    def __init__(self):
-        # not sure what the "state" of a rule object should be
-        pass
+        while True:
+            choice = input(prompt).lower()
+            if choice in Player.CHOICES:
+                break
 
-    # not sure where "compare" goes yet
-    def compare(self, move1, move2):
-        pass
+            print(f"Sorry, {choice} is not valid")
+
+        self.move = choice
 
 
 class RPSGame:
     def __init__(self):
-        self._human = Player()
-        self._computer = Player()
+        self._human = Human()
+        self._computer = Computer()
+
+    def _display_welcome_message(self):
+        print("Welcome to Rock Paper Scissors!")
+
+    def _display_goodbye_message(self):
+        print("Thanks for playing Rock Paper Scissors. Goodbye!")
+
+    def _human_wins(self):
+        human_move = self._human.move
+        computer_move = self._computer.move
+
+        return (
+            (human_move == "rock" and computer_move == "scissors")
+            or (human_move == "paper" and computer_move == "rock")
+            or (human_move == "scissors" and computer_move == "paper")
+        )
+
+    def _computer_wins(self):
+        human_move = self._human.move
+        computer_move = self._computer.move
+
+        return (
+            (computer_move == "rock" and human_move == "scissors")
+            or (computer_move == "paper" and human_move == "rock")
+            or (computer_move == "scissors" and human_move == "paper")
+        )
+
+    def _display_winner(self):
+        print(f"You chose: {self._human.move}")
+        print(f"The computer chose: {self._computer.move}")
+
+        if self._human_wins():
+            print("You win!")
+        elif self._computer_wins():
+            print("Computer wins!")
+        else:
+            print("It's a tie")
 
     def play(self):
-        display_welcome_message()
-        self._human.choose()
-        self._computer.choose()
-        display_winner()
-        display_goodbye_message()
+        self._display_welcome_message()
+
+        while True:
+            self._human.choose()
+            self._computer.choose()
+            self._display_winner()
+            if not self._play_again():
+                break
+
+        self._display_goodbye_message()
+
+    def _play_again(self):
+        answer = input("Would you like to play again? (y/n) ")
+        return answer.lower().startswith("y")
+
+
+RPSGame().play()

414711245b69e6d974dd1c91fdba26d40ca494dc
Delvon <delvonj21@gmail.com>
2025-06-02 08:31:02 +0200

    added practice problems

---
+++ PY120/practice_problems/Easy2/game.py
@@ -0,0 +1,14 @@
+# Update this code so that Bingo inherits the play method from the Game class.
+
+
+class Game:
+    def play(self):
+        return "Start the game!"
+
+
+class Bingo(Game):
+    pass
+
+
+game = Bingo()
+print(game.play())

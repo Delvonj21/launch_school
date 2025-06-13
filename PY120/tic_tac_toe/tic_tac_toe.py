613db0a8a8d6294b61bf797522c644842b69c909
Delvon <delvonj21@gmail.com>
2025-06-13 08:02:43 +0200

    finshed TTT game

--- PY120/tic_tac_toe/tic_tac_toe.py
+++ PY120/tic_tac_toe/tic_tac_toe.py
@@ -1,4 +1,9 @@
 import random
+import os
+
+
+def clear_screen():
+    os.system("clear")
 
 
 class Square:
@@ -19,6 +24,9 @@ class Square:
     @marker.setter
     def marker(self, marker):
         self._marker = marker
+
+    def is_unused(self):
+        return self.marker == Square.INITIAL_MARKER
 
 
 class Board:
@@ -49,21 +57,25 @@ class Board:
     def mark_square_at(self, key, marker):
         self.squares[key].marker = marker
 
+    def unused_squares(self):
+        return [key for key, square in self.squares.items() if square.is_unused()]
 
-class Row:
-    def __init__(self):
-        # We need some way to identify a row of 3 squares
-        pass
+    def is_full(self):
+        return len(self.unused_squares()) == 0
+
+    def count_markers_for(self, player, keys):
+        markers = [self.squares[key].marker for key in keys]
+        return markers.count(player.marker)
+
+    def display_with_clear(self):
+        clear_screen()
+        print("\n")
+        self.display()
 
 
 class Player:
     def __init__(self, marker):
         self.marker = marker
-
-    def play(self):
-        # We need a way for each player to play the game.
-        # Do we need access to the board?
-        pass
 
 
 class Human(Player):
@@ -77,6 +89,17 @@ class Computer(Player):
 
 
 class TTTGame:
+    POSSIBLE_WINNING_ROWS = (
+        (1, 2, 3),  # top row of board
+        (4, 5, 6),  # center row of board
+        (7, 8, 9),  # bottom row of board
+        (1, 4, 7),  # left column of board
+        (2, 5, 8),  # middle column of board
+        (3, 6, 9),  # right column of board
+        (1, 5, 9),  # diagonal: top-left to bottom-right
+        (3, 5, 7),  # diagonal: top-right to bottom-left
+    )
+
     def __init__(self):
         self.board = Board()
         self.human = Human()
@@ -84,43 +107,51 @@ class TTTGame:
 
     def play(self):
         self.display_welcome_message()
+        self.board.display()
 
         while True:
-            self.board.display()
-
             self.human_moves()
-            self.board.display()  # so we can see the human's move
             if self.is_game_over():
                 break
 
             self.computer_moves()
-            self.board.display()  # so we can see the computer's move
             if self.is_game_over():
                 break
 
-            break  # Execute loop only once for now
+            self.board.display_with_clear()
 
-        self.board.display()
+        self.board.display_with_clear()
         self.display_results()
         self.display_goodbye_message()
 
     def display_welcome_message(self):
+        clear_screen()
         print("Welcome to Tic Tac Toe!")
+        print()
 
     def display_goodbye_message(self):
         print("Thanks for playing Tic Tac Toe! Goodbye!")
 
     def display_results(self):
-        # Show the results of this game (win, lose, tie).
-        pass
+        if self.is_winner(self.human):
+            print("You won! Congratulations!")
+        elif self.is_winner(self.computer):
+            print("I won! I won! Take that, human!")
+        else:
+            print("A tie game. How boring.")
 
     def human_moves(self):
         choice = None
+        valid_choices = self.board.unused_squares()
         while True:
-            choice = input("Choose a square between 1 and 9: ")
+            choices_list = [str(choice) for choice in valid_choices]
+            choices_str = ", ".join(choices_list)
+            prompt = f"Choose a square ({choices_str}): "
+            choice = input(prompt)
+
             try:
                 choice = int(choice)
-                if 1 <= choice <= 9:
+                if choice in valid_choices:
                     break
             except ValueError:
                 pass
@@ -131,11 +162,24 @@ class TTTGame:
         self.board.mark_square_at(choice, self.human.marker)
 
     def computer_moves(self):
-        choice = random.randint(1, 9)
+        valid_choices = self.board.unused_squares()
+        choice = random.choice(valid_choices)
         self.board.mark_square_at(choice, self.computer.marker)
 
     def is_game_over(self):
-        # We'll start by assuming the game never ends.
+        return self.board.is_full() or self.someone_won()
+
+    def three_in_a_row(self, player, row):
+        return self.board.count_markers_for(player, row) == 3
+
+    def someone_won(self):
+        return self.is_winner(self.human) or self.is_winner(self.computer)
+
+    def is_winner(self, player):
+        for row in TTTGame.POSSIBLE_WINNING_ROWS:
+            if self.three_in_a_row(player, row):
+                return True
+
         return False
 
 
